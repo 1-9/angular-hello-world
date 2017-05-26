d377a4205eeefe386ee4cf67a16d6f9c18942425
Alex <dima332@list.ru>
2017-05-26 20:21:56 +0300

    несколько изменений в компонент

--- src/app/hll-wrld/hll-wrld.component.ts
+++ src/app/hll-wrld/hll-wrld.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hll-wrld',
-  templateUrl: './hll-wrld.component.html',
-  styleUrls: ['./hll-wrld.component.css']
+    selector: 'app-hll-wrld',
+    templateUrl: './hll-wrld.component.html',
+    styleUrls: ['./hll-wrld.component.css']
 })
 export class HllWrldComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
